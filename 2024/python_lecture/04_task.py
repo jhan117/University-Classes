7aa39d778c474beb323a5c0e5a27185d087db70d
2001Kaye <jhan117@naver.com>
2024-03-29 13:48:52 +0900

    [03-29] Edit outcome function

--- 2024/python_lecture/04_task.py
+++ 2024/python_lecture/04_task.py
@@ -9,44 +9,49 @@ GREEN = "\033[1;32m"
 YELLOW = "\033[1;33m"
 # 상수
 POSSIBLE_ACTIONS = ["가위", "바위", "보"]
+WIN = f"{GREEN}* 축하합니다. 당신이 이겼습니다.{END}"
+LOSE = f"{RED}* 컴퓨터가 이겼습니다.{END}"
+DRAW = f"{WHITE}* 비겼습니다.{END}"
 
 
-def outcome(user_action, computer_action):
+def outcome(user_action, computer_action, counts):
     """
-    승리시 True 반환
+    출력 및 count 반환
+    counts = [win, lose, draw]
     """
-    WIN = f"{GREEN}* 축하합니다. 당신이 이겼습니다.{END}"
-    LOSE = f"{RED}* 컴퓨터가 이겼습니다.{END}"
 
-    match user_action:
-        case "가위":
-            if computer_action == "보":
-                print(WIN)
-                return True
-            else:
-                print(LOSE)
-                return False
-        case "바위":
-            if computer_action == "가위":
-                print(WIN)
-                return True
-            else:
-                print(LOSE)
-                return False
-        case "보":
-            if computer_action == "바위":
-                print(WIN)
-                return True
-            else:
-                print(LOSE)
-                return False
+    if user_action == computer_action:
+        print(DRAW)
+        counts[2] += 1
+    else:
+        match user_action:
+            case "가위":
+                if computer_action == "보":
+                    print(WIN)
+                    counts[0] += 1
+                else:
+                    print(LOSE)
+                    counts[1] += 1
+            case "바위":
+                if computer_action == "가위":
+                    print(WIN)
+                    counts[0] += 1
+                else:
+                    print(LOSE)
+                    counts[1] += 1
+            case "보":
+                if computer_action == "바위":
+                    print(WIN)
+                    counts[0] += 1
+                else:
+                    print(LOSE)
+                    counts[1] += 1
+    return counts
 
 
 while True:
-    # 총 승패 횟수
-    win_count = 0
-    lose_count = 0
-    draw_count = 0
+    # 총 승패 횟수 : win, lose, draw
+    counts = [0, 0, 0]
 
     # 횟수 입력
     round_input = input("가위, 바위, 보 게임을 몇 회 실시하겠습니까? (press q to quit) : ")
@@ -83,28 +88,18 @@ while True:
                 print("다시 입력해주세요...")
                 continue
 
-            # 컴퓨터 랜덤 출력
+            # 컴퓨터 랜덤 및 각 선택 출력
             computer_action = choice(POSSIBLE_ACTIONS)
-
             print(f"YOU : {user_action}, COMPUTER : {computer_action}")
 
             # 승패 결정
-            if user_action == computer_action:
-                print(f"{WHITE}* 비겼습니다.{END}")
-                draw_count += 1
-                break
-            else:
-                if outcome(user_action, computer_action):
-                    win_count += 1
-                    break
-                else:
-                    lose_count += 1
-                    break
+            counts = outcome(user_action, computer_action, counts)
+            break
 
     # 게임 종료
     print("=" * 80)
     print(">>> 게임이 종료되었습니다.\n>>> 최종 결과는 다음과 같습니다.")
-    print(f"컴퓨터가 이긴 횟수 : {RED}{lose_count}{END}\n비긴 횟수 : {WHITE}{
-          draw_count}{END}\n당신이 이긴 횟수 : {GREEN}{win_count}{END}")
+    print(f"컴퓨터가 이긴 횟수 : {RED}{counts[1]}{END}\n비긴 횟수 : {WHITE}{
+          counts[2]}{END}\n당신이 이긴 횟수 : {GREEN}{counts[0]}{END}")
 
     print("=" * 80)
