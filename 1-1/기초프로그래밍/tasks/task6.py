bda879e812936f14d3871df9f4c70312a1272747
2001Kaye <jhan117@naver.com>
2024-05-31 00:51:47 +0900

    Task6: Feat
    - notebook으로 도형과 펜 변경 기능 추가
    - 커서 모양 변경 기능 추가
    - 펜과 지우개 기능 추가
    - undo 기능, 선 굵기 변경 추가

--- 1-1/기초프로그래밍/tasks/task6.py
+++ 1-1/기초프로그래밍/tasks/task6.py
@@ -17,12 +17,17 @@ class PaintBoard:
         # 초기 변수
         self.works = []
         self.prev_pos = (0, 0)
+        self.is_pen = True
+        self.temp_lines = []
 
         # 위젯들 생성 및 배치
         self.header = ttk.Frame(self.root)
         self.draw_canvas = tk.Canvas(self.root, bg="white")
-        self.footer = ttk.Frame(self.root)
-        self.shape_var = tk.StringVar(None, "")
+        self.footer = ttk.Notebook(self.root)
+        self.tool_var = tk.IntVar(None, 1)  # 0: 지우개, 1: 펜
+        self.size_var = tk.IntVar(None, 3)  # 1: 얇게, 3: 보통, 5: 굵게
+        self.pen_color_var = tk.StringVar(None, "")
+        self.shape_var = tk.StringVar(None, "line")
         self.color_var = tk.StringVar(None, "")
         self.check_fill_var = tk.BooleanVar()
         self.make_widgets()
@@ -31,19 +36,51 @@ class PaintBoard:
         self.draw_canvas.bind("<Button-1>", self.press_mouse)  # 클릭
         self.draw_canvas.bind("<B1-Motion>", self.motion_mouse)  # 왼클릭하면서 움직임
         self.draw_canvas.bind("<ButtonRelease-1>", self.release_mouse)  # 뗌
+        self.footer.bind("<<NotebookTabChanged>>", self.change_cursor)  # tab 변경시
 
         self.root.mainloop()
 
     def press_mouse(self, e):
         self.prev_pos = (e.x, e.y)
+        if self.is_pen:
+            self.works.append(self.temp_lines)
 
     def motion_mouse(self, e):
-        self.create_polygon(e)
+        if self.is_pen:
+            if self.tool_var.get():
+                color = self.pen_color_var.get() if self.pen_color_var.get() else "black"
+            else:
+                color = "white"
+            temp_line = self.draw_canvas.create_line(*self.prev_pos, e.x, e.y, fill=color, width=self.size_var.get())
+            self.temp_lines.append(temp_line)
+            self.prev_pos = (e.x, e.y)
+        else:
+            self.create_polygon(e)
 
     def release_mouse(self, e):
-        drew_shape = self.create_polygon(e, is_moving=False)
-        if drew_shape:
-            self.works.append(drew_shape)
+        if self.is_pen:
+            self.temp_lines = []
+        else:
+            drew_shape = self.create_polygon(e, is_moving=False)
+            if drew_shape:
+                self.works.append(drew_shape)
+
+    def change_cursor(self, e):
+        if self.footer.index("current"):
+            self.is_pen = False
+            self.draw_canvas.config(cursor="tcross")
+        else:
+            self.is_pen = True
+            self.draw_canvas.config(cursor="dotbox")
+
+    def undo_work(self):
+        if self.works:
+            work = self.works.pop()
+            if isinstance(work, list):
+                for idx in work:
+                    self.draw_canvas.delete(idx)
+            else:
+                self.draw_canvas.delete(work)
 
     def create_polygon(self, e, is_moving=True):
         # 변수 설정
@@ -69,20 +106,37 @@ class PaintBoard:
         # == 생성 == #
         # in header
         all_delete_btn = ttk.Button(self.header, text="모두 삭제", command=lambda: self.draw_canvas.delete(tk.ALL))
-        undo_btn = ttk.Button(self.header, text="Undo",
-                              command=lambda: self.draw_canvas.delete(self.works.pop()) if self.works else None)
+        undo_btn = ttk.Button(self.header, text="Undo", command=self.undo_work)
         # in footer
-        shape_frame = ttk.LabelFrame(self.footer, text="모양 설정")
-        color_frame = ttk.LabelFrame(self.footer, text="색깔 설정")
-        fill_frame = ttk.LabelFrame(self.footer, text="색깔 채움 설정")
-        # footer_shape
+        footer_pen = ttk.Frame(self.footer)
+        footer_poly = ttk.Frame(self.footer)
+        self.footer.add(footer_pen, text="그리기", padding=5)
+        self.footer.add(footer_poly, text="도형", padding=5)
+
+        # footer_pen
+        tools_frame = ttk.LabelFrame(footer_pen, text="도구 설정")
+        color_frame_pen = ttk.LabelFrame(footer_pen, text="색깔 설정")
+        size_frame = ttk.LabelFrame(footer_pen, text="크기 설정")
+        # footer_poly
+        shape_frame = ttk.LabelFrame(footer_poly, text="모양 설정")
+        color_frame = ttk.LabelFrame(footer_poly, text="색깔 설정")
+        fill_frame = ttk.LabelFrame(footer_poly, text="색깔 채움 설정")
+        # footer_pen_radiobutton
+        ttk.Radiobutton(tools_frame, variable=self.tool_var, text="펜", value=1).pack(**args)
+        ttk.Radiobutton(tools_frame, variable=self.tool_var, text="지우개", value=0).pack(**args)
+        ttk.Radiobutton(size_frame, variable=self.size_var, text="얇게", value=1).pack(**args)
+        ttk.Radiobutton(size_frame, variable=self.size_var, text="보통", value=3).pack(**args)
+        ttk.Radiobutton(size_frame, variable=self.size_var, text="굵게", value=5).pack(**args)
+        # footer_poly_radiobutton
         shape_list = {"line": "직선", "rect": "사각형", "oval": "타원"}
         color_list = {"red": "빨강", "green": "초록", "blue": "파랑"}
         var_list = {"shape": [shape_frame, self.shape_var, shape_list],
                     "color": [color_frame, self.color_var, color_list]}
-        for values in var_list.values():
+        for key, values in var_list.items():
             for v, t in values[2].items():
                 ttk.Radiobutton(values[0], variable=values[1], text=t, value=v).pack(**args)
+                if key == "color":
+                    ttk.Radiobutton(color_frame_pen, variable=self.pen_color_var, text=t, value=v).pack(**args)
         # footer_fill
         check_fill = ttk.Checkbutton(fill_frame, text="색깔 채움 여부", variable=self.check_fill_var)
 
@@ -99,6 +153,10 @@ class PaintBoard:
         shape_frame.pack(**args)
         color_frame.pack(**args, padx=5)
         fill_frame.pack(**args)
+        # footer_pen
+        tools_frame.pack(**args)
+        color_frame_pen.pack(**args, padx=5)
+        size_frame.pack(**args)
         # footer_fill
         check_fill.pack()
 
