d4cc194b137e0b63de30cfae93ddd05520306749
2001Kaye <jhan117@naver.com>
2024-10-14 11:09:16 +0900

    ♻️ C프로그래밍 과제 refactor

--- 1-2/C_Programming/Assignments/Assignment_1/main1.c
+++ 1-2/C_Programming/Assignments/Assignment_1/main1.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-	char num1 = 0x7f;
-	int num2 = 0x7fffffff;
+    char num1 = 0x7f;
+    int num2 = 0x7fffffff;
 
-	printf("char형 변수 초깃값: %X %d\n", num1, num1);
-	printf("오버플로우: %X %d\n", ++num1, num1);
-	printf("언더플로우: %X %d\n", --num1, num1);
+    printf("char형 변수 초깃값: %X %d\n", num1, num1);
+    printf("오버플로우: %X %d\n", ++num1, num1);
+    printf("언더플로우: %X %d\n", --num1, num1);
 
-	printf("int형 변수 초깃값: %X %d\n", num2, num2);
-	printf("오버플로우: %X %d\n", ++num2, num2);
-	printf("언더플로우: %X %d\n", --num2, num2);
+    printf("int형 변수 초깃값: %X %d\n", num2, num2);
+    printf("오버플로우: %X %d\n", ++num2, num2);
+    printf("언더플로우: %X %d\n", --num2, num2);
 
-	return 0;
+    return 0;
 }
